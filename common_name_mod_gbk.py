c77a418668540983fd83c634862a023a776accc0
James Matsumura <james.matsumura@gmail.com>
2017-02-23 17:15:35 -0500

    Modifying how EC numbers are mapped so that they replace products correctly and do nothing for multimaps.

--- common_name_mod_gbk.py
+++ common_name_mod_gbk.py
@@ -3,54 +3,7 @@
 # Script to convert product names in GBK file to a different set prescribed by metadata.
 
 import sys, re
-
-# Function to print the product name to a file. Accepts the product name
-# and the handle for the outfile. Will process multi-line name if the
-# characters-per-line exceeds 80.
-def printName(name,out): # need some handling in case of multi-line
-    base = " " * 21 + "/product=" # first line
-    blank_base = " " * 21 # non-first lines
-
-    # Here replace some common errors found in product names
-    name = name.replace('sulphide','sulfide')
-    name = name.replace('sulphur','sulfur')
-    name = name.replace(' fibre ',' fiber ')
-
-    if len(name) < 48: # good to go, can print on one line
-        final = '%s"%s"\n' % (base,name)
-        out.write(final)
-
-    # If greater than that length, need to do a multi-line print. Note
-    # that this approach does not absolutely maximize line length, but 
-    # arbitrarily does not permit this region to extend beyond the ~70 
-    # character mark. 
-    else: 
-
-        max_len = 47
-        words = name.split(' ')
-        multi_line = [] # store all the lines in the product qualifier
-        single_line = [] # store just the current line being built
-
-        for x in words:
-            if not (len(' '.join(single_line)) + len(x)) > max_len: # if i can fit, add it
-                single_line.append(x)
-            else: # too long, add to multi-line and build a new one
-                multi_line.append(' '.join(single_line))
-                single_line = []
-                single_line.append(x)
-
-        multi_line.append(' '.join(single_line))
-
-        for j in range(0,len(multi_line)):
-            line = ""
-            if j == 0: # first line
-                line = '%s"%s\n' % (base,multi_line[j])
-            elif j == (len(multi_line)-1): # last line
-                line = '%s%s"\n' % (blank_base,multi_line[j])
-            else: # middle line
-                line = '%s%s\n' % (blank_base,multi_line[j])
-            out.write(line)
-
+from shared_fxns import printName
 
 # Function to build a name from a multi-line product in a GBK file. 
 # Accepts the list of lines consisting of the product names, which
--- ec_numbers_mod_gbk.py
+++ ec_numbers_mod_gbk.py
@@ -5,6 +5,8 @@
 # for a enzyme.dat file. 
 
 import sys, re
+from collections import defaultdict
+from shared_fxns import printName
 
 metadata = str(sys.argv[1])
 out_dir = str(sys.argv[2])
@@ -16,11 +18,16 @@ ed = open(enzyme_file,'r')
 regex_for_ec_num = r'\s+/EC_number="(.*)"'
 regex_for_ec_id = r'^ID\s+(.*)$'
 regex_for_ec_transferred = r'^DE\s+Transferred entry:\s(.*)$'
+regex_for_product_name = r'^DE\s+(.*)$'
+regex_for_gbk_product = '/product="(.*)'
 
-transferred_ec = {} # dict for ECs that have been updated
+transferred_ec,old_ec_product,new_ec_product = ({} for i in range(3)) # dict for ECs that have been updated
+# maintain a dictionary for output and tell users which were deleted, transferred, and multimapped
+out_map = defaultdict(dict)
 
 # First, extract all the relevant information from the enzyme.dat file
 id = "" # single EC number ID
+product_list = [] # use a list to handle multiline products
 
 for line in ed:
     if line.startswith('ID'): # grab most recent ID
@@ -29,12 +36,25 @@ for line in ed:
         all_ecs = re.search(regex_for_ec_transferred,line).group(1)
         indiv_ecs = all_ecs.split(' ')
         if len(indiv_ecs) > 1: # hit multiple, need manual curation let the discrep file catch
-            transferred_ec[id] = id
+            out_map['multi-mapped'][id] = ' '.join(indiv_ecs)
         else:
             ec = all_ecs
             if not ec[-1:].isdigit():
                 ec = ec[:-1] # trim commas and periods
             transferred_ec[id] = ec # assign to new transferred entry EC
+            out_map['single-mapped'][id] = ec
+    elif line.startswith('DE'): # found a product name if starting with DE and not transferred entry
+        # Know that the previous ID extracted is what this product name is tied to.
+        product = re.search(regex_for_product_name,line).group(1) 
+
+        if product.endswith('.'): # got the full product    
+            product_list.append(product)
+            new_ec_product[id] = ''.join(product_list)
+            product_list = [] # reinitialize
+
+        else: # not the full name, just grab the first part
+            product_list.append(product)
+
 
 # Iterate over the metadata file, one line per GBK to process
 for line in md:
@@ -56,22 +76,78 @@ for line in md:
     for l in ec_numbers:
         l = l.strip('\n')
         ecs_to_delete.append(l)
-    delete_us = set(ecs_to_delete)
+    delete_us = set()
+    if len(ecs_to_delete) > 0:
+        delete_us = set(ecs_to_delete)
 
     # Make a new file name for the outfile
     gbk_out = "%s/%s/ec_numbers_mod.gbk" % (out_dir,locus)
     outfile = open(gbk_out,'w')
 
+    ec_list = [] # gather all EC numbers and note which have changed
+    product_list = [] # capture multiline products for outfile
+    rename_product,within_product = (False for i in range(2))
+    mod_ec,curr_ec = ("" for i in range(2))
+
     for l in gbk:
 
         if "/EC_number=" in l: # found the EC number
-            #curr_ec = re.search(regex_for_ec_num,l).group(1)
-            #if curr_ec in transferred_ec: # if updated EC, replace
-            #    base = " " * 21 # no replacing digits easy, just build new string
-            #    ec_str = '/EC_number="%s"\n' % (transferred_ec[curr_ec])
-            #    l = "%s%s" % (base,ec_str)
+            curr_ec = re.search(regex_for_ec_num,l).group(1)
+            ec_list.append(curr_ec)
+
+            if curr_ec in transferred_ec: # if updated EC and single mapped, replace
+                base = " " * 21 # no replacing digits easily, just build new string
+                ec_str = '/EC_number="%s"\n' % (transferred_ec[curr_ec])
+                l = "%s%s" % (base,ec_str)
+                rename_product = True
+
             mod_ec = re.search(regex_for_ec_num,l).group(1) # potentially modified EC
+
             if mod_ec not in delete_us: # only print those that aren't to be deleted
                 outfile.write(l)
+        
         else:
-            outfile.write(l)
+            if rename_product == True:
+                if "/product=" in l: # skip over old product, write a new one
+                    printName(new_ec_product[mod_ec],outfile)
+                    tmp_line = l.strip()
+
+                    if not tmp_line.endswith('"'): # multiline product
+                        within_product = True
+                        product_list.append(re.search(regex_for_gbk_product,l).group(1))
+
+                    else: # single line product
+                        product = re.search(regex_for_gbk_product,l).group(1)
+                        product = product[:-1]
+                        old_ec_product[curr_ec] = product # put the old EC number in 
+
+                elif within_product == True:
+                    tmp_line = l.strip()
+
+                    if tmp_line.endswith('"'): # found the end of the product
+                        tmp_line = tmp_line[:-1]
+                        product_list.append(tmp_line)
+                        old_ec_product[curr_ec] = ''.join(product_list)
+
+                        within_product = False
+                        product_list = [] # reinitialize
+                    
+                    else:
+                        product_list.append(tmp_line)
+
+                elif "/translation=" in l or "/transl_table=" in l: # reached the end of the product, resolve it and leave
+                    outfile.write(l)
+                    rename_product = False
+
+            else: # if don't need to rename, just keep the current structure
+                outfile.write(l)
+
+    ec_hist = "%s/%s/ec_history.tsv" % (out_dir,locus)
+    histfile = open(ec_hist,'w')
+
+    for map_type,id_key in out_map.items():
+        for original_id,new_id in out_map[map_type].items():
+            if original_id in ec_list and map_type == 'single-mapped':
+                histfile.write("%s\t%s\t%s\t%s\t%s\n" % (map_type,original_id,old_ec_product[original_id],new_id,new_ec_product[new_id]))
+            else: # write out multimaps
+                histfile.write("%s\t%s\trequires curation for which of these to move to:\t%s") % (map_type,original_id,new_id)
